0b6f6fa27e457b30c76fa5ec2e2f1a5108dcaa42
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-03-07 13:43:28 +0100

    Extra loop on lepton flavours

--- bin/MadGraph5_aMCatNLO/cards/production/13TeV/GamGamToSlSl_SlToLN1/GamGamToSlSl_SlToLN1.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/13TeV/GamGamToSlSl_SlToLN1/GamGamToSlSl_SlToLN1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+channels=(e mu tau)
 masses_sl=(100 150 200 250 300 350)
 dmasses=(10 20 30 40 50 60)
 template=GamGamToSlSl_SlToLN1_template
@@ -9,12 +10,14 @@ postfix=(customizecards.dat run_card.dat proc_card.dat extramodels.dat)
 
 for msl in ${masses_sl[*]}; do
     for dm in ${dmasses[*]}; do
-        dir=${template/template/M$msl-$dm}
-        echo "Processing masses point (m_sl=$msl, m_sl-m_n1=$dm) -> $dir"
-        mkdir $dir
-        mn1=$((msl-dm)) # compute the neutralino mass from masses hierarchy
-        for pf in ${postfix[*]}; do
-            sed "s/<<<MSL>>>/${msl}/g; s/<<<MN1>>>/${mn1}/g" $template/$prefix$pf > $dir/${dir}_$pf
+        for lep in ${channels[*]}; do
+            dir=${template/template/${lep}${lep}_M$msl-$dm}
+            echo "Processing masses point (m_sl=$msl, m_sl-m_n1=$dm) for $lep-channel -> $dir"
+            mkdir $dir
+            mn1=$((msl-dm)) # compute the neutralino mass from masses hierarchy
+            for pf in ${postfix[*]}; do
+                sed "s/<<<MSL>>>/${msl}/g; s/<<<MN1>>>/${mn1}/g; s/<<<LEP>>>/${lep}/g" $template/$prefix$pf > $dir/${dir}_$pf
+            done
         done
     done
 done
