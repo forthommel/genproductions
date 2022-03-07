4f95db7df1ddba5e8ba108b269135ba2857d08a4
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-03-07 14:17:07 +0100

    Removed extramodels, changed output process name

--- bin/MadGraph5_aMCatNLO/cards/production/13TeV/GamGamToSlSl_SlToLN1/GamGamToSlSl_SlToLN1.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/13TeV/GamGamToSlSl_SlToLN1/GamGamToSlSl_SlToLN1.sh
@@ -8,7 +8,7 @@ dmasses=(10 20 30 40 50 60) # mass differences between slepton and neutralino
 template=GamGamToSlSl_SlToLN1_template
 
 prefix=${template/template/}
-postfix=(customizecards.dat run_card.dat proc_card.dat extramodels.dat)
+postfix=(customizecards.dat proc_card.dat run_card.dat)
 
 for msl in ${masses_sl[*]}; do
     for dm in ${dmasses[*]}; do
@@ -18,7 +18,7 @@ for msl in ${masses_sl[*]}; do
             mkdir $dir
             mn1=$((msl-dm)) # compute the neutralino mass from masses hierarchy
             for pf in ${postfix[*]}; do
-                sed "s/<<<MSL>>>/${msl}/g; s/<<<MN1>>>/${mn1}/g; s/<<<LEP>>>/${lep}/g" $template/$prefix$pf > $dir/${dir}_$pf
+                sed "s/<<<MSL>>>/${msl}/g; s/<<<MN1>>>/${mn1}/g; s/<<<LEP>>>/${lep}/g; s/<<<OUTPUT>>>/${dir}/g" $template/$prefix$pf > $dir/${dir}_$pf
             done
         done
     done
